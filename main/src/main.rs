d07b65294a1d3421cb6d6fda643612efcbc3fbff
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-01-24 18:49:48 -0500

    signatures work

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,4 +1,5 @@
 use babyjubjub_rs::PrivateKey;
+use num_bigint::{BigInt, RandBigInt, Sign, ToBigInt};
 fn main() {
     let sk = PrivateKey::import(
         hex::decode("0001020304050607080900010203040506070809000102030405060708090001")
@@ -6,5 +7,8 @@ fn main() {
     )
     .unwrap();
     let a= sk.public();
-    // println!("secret key is {:?}", s);
+    let sig = sk.sign(69.to_bigint().unwrap()).unwrap();
+    println!("secret key is {:?} and public key is {:?}", sk.scalar_key(), a);
+    println!("Signature is R8 : {:?} , S : {:?}", sig.r_b8, sig.s);
+
 }
