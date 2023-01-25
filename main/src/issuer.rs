8d430087d7fdbb2b57a837760510e8d9f80c598b
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-01-25 18:44:13 -0500

    issuer has more features

--- main/src/issuer.rs
+++ main/src/issuer.rs
@@ -1,10 +1,33 @@
 use num_bigint::{BigInt, RandBigInt, Sign, ToBigInt};
 use babyjubjub_rs::{POSEIDON, Fr, Point, PrivateKey};
-
+use ff::PrimeField;
+use time::Timespec;
+extern crate time;
 pub struct Issuer {
     pub privkey: PrivateKey,
     pub pubkey_point: Point,
     pub address: Fr //Address is poseidon([pubkey_point.x, pubkey_point.y])
+}
+
+pub struct HoloTimestamp {
+    timestamp: Fr
+}
+
+impl HoloTimestamp {
+    pub fn from_timespec(t: Timespec) -> HoloTimestamp {
+        let sec1900 = t.sec - 2208988800; // 2208988800000 is 70 year offset; Unix timestamps below 1970 are negative and we want to allow from 1900
+        // return Err(String::from("Error parsing time::Timespec object"));
+        // Ok(HoloTimestamp { timestamp : Fr::from(sec1900) });
+        return HoloTimestamp { timestamp : Fr::from_str(&sec1900.to_string()).unwrap() }
+    }
+}
+
+pub struct Leaf {
+    pub address: Fr,
+    pub secret: Fr,
+    pub custom_fields: [Fr; 2],
+    pub iat: Fr, // Timestamp issued at, offset to 1900 instead of standard unix 1970
+
 }
 impl Issuer {
     pub fn from_privkey(privkey: &str) -> Issuer{
@@ -26,8 +49,8 @@ impl Issuer {
     }
 
     // Returns poseidon([issuer address, random secret, custom_fields[0], custom_fields[1], current timestamp as days since 1900, scope (never/seldom used; set to 0 by default)])
-    pub fn create_leaf(custom_fields: &[Fr; 2]) {
-        // POSEIDON.hash(vec![pk.x, pk.y]).unwrap()
+    pub fn create_leaf(&self, custom_fields: &[Fr; 2]) -> Fr {
+        POSEIDON.hash(vec![self.address]).unwrap()
     }
 }
 
