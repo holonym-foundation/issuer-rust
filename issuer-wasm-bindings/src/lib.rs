26189153c6a5c284c2eefe36aa83079e4208fec0
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-01-28 15:22:47 -0500

    issuing credentials works in wasm

--- issuer-wasm-bindings/src/lib.rs
+++ issuer-wasm-bindings/src/lib.rs
@@ -1,18 +1,23 @@
 use wasm_bindgen::prelude::*;
-use issuer::HoloTimestamp;
+use issuer::Issuer;
+extern crate console_error_panic_hook;
+use std::panic;
 
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> String {
-    "hey".to_string()
+pub fn issue(private_key: String, field1: String, field2: String) -> String {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let iss = Issuer::from_privkey(&private_key);
+    let sig = iss.issue(["12345678".to_string(), "23456789".to_string()]).unwrap();
+    serde_json::to_string(&sig).unwrap()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        // assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         // let result = add(2, 2);
+//         // assert_eq!(result, 4);
+//     }
+// }
